d60e9f49e5ae620cf37ea5bd9df6394cc6e4c65e
KIMLENDING <gotslaakaak@gmail.com>
2024-12-15 07:43:29 +0900

    주간 시작일 및 종료일 계산 로직 개선: 변수 이름 변경 및 코드 정리

--- app/api/user/SessionWeek/route.ts
+++ app/api/user/SessionWeek/route.ts
@@ -5,19 +5,18 @@ import { getServerSession } from "next-auth"
 import User from "@/models/User"
 function getWeekRange() {
     const today = new Date(new Date()); // 오늘 날짜 (한국 시간 기준)
-    const dayOfWeek = today.getDay(); // 요일 (0: 일요일 ~ 6: 토요일)
-    // 일요일 00:00:00 계산
-    const startOfWeek = new Date(today);
-    startOfWeek.setDate(today.getDate() - dayOfWeek);
-    startOfWeek.setHours(0, 0, 0, 0);
-    console.log(startOfWeek)
+    const sunday = new Date(today);
+    sunday.setDate(sunday.getDate() - sunday.getDay()); // 일요일 날짜 구하기
+    sunday.setHours(0, 0, 0, 0);
+
+    console.log(sunday)
 
     // 토요일 23:59:59 계산
-    const endOfWeek = new Date(startOfWeek);
-    endOfWeek.setDate(startOfWeek.getDate() + 6);
-    endOfWeek.setHours(23, 59, 59, 999);
+    const saturday = new Date(sunday);
+    saturday.setDate(sunday.getDate() + 6);
+    saturday.setHours(23, 59, 59, 999);
 
-    return { startOfWeek, endOfWeek };
+    return { sunday, saturday };
 }
 
 
@@ -36,15 +35,15 @@ export const GET = async (request: NextRequest) => {
         if (!user) {
             return NextResponse.json({ message: "User not found" }, { status: 404 });
         }
-        const { startOfWeek, endOfWeek } = getWeekRange();
+        const { sunday, saturday } = getWeekRange();
         const sessions = await ExerciseSession.find({
             userId: user._id,
-            createdAt: { $gte: startOfWeek, $lte: endOfWeek },
+            createdAt: { $gte: sunday, $lte: saturday },
         }).sort({ createdAt: 1 });
         if (!sessions) {
             return NextResponse.json({ message: "주간 데이터가 없습니다." }, { status: 200 });
         }
-        return NextResponse.json({ sessions, startOfWeek, message: "주간 데이터" }, { status: 200 });
+        return NextResponse.json({ sessions, sunday, message: "주간 데이터" }, { status: 200 });
     } catch (error) {
         console.error("Error fetching exercise session:", error);
         return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
