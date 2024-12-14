102302fcce72fb3cbbd403fd3353bbdb7e0df392
KIMLENDING <gotslaakaak@gmail.com>
2024-12-15 07:37:32 +0900

    주간 시작일 계산 로직 수정 및 응답에 주간 시작일 추가

--- app/api/user/SessionWeek/route.ts
+++ app/api/user/SessionWeek/route.ts
@@ -4,13 +4,13 @@ import { NextRequest, NextResponse } from "next/server"
 import { getServerSession } from "next-auth"
 import User from "@/models/User"
 function getWeekRange() {
-    const today = new Date(new Date().toLocaleString("ko-KR", { timeZone: "Asia/Seoul" })); // 오늘 날짜 (한국 시간 기준)
+    const today = new Date(new Date()); // 오늘 날짜 (한국 시간 기준)
     const dayOfWeek = today.getDay(); // 요일 (0: 일요일 ~ 6: 토요일)
-
     // 일요일 00:00:00 계산
     const startOfWeek = new Date(today);
     startOfWeek.setDate(today.getDate() - dayOfWeek);
     startOfWeek.setHours(0, 0, 0, 0);
+    console.log(startOfWeek)
 
     // 토요일 23:59:59 계산
     const endOfWeek = new Date(startOfWeek);
@@ -44,7 +44,7 @@ export const GET = async (request: NextRequest) => {
         if (!sessions) {
             return NextResponse.json({ message: "주간 데이터가 없습니다." }, { status: 200 });
         }
-        return NextResponse.json({ sessions, message: "주간 데이터" }, { status: 200 });
+        return NextResponse.json({ sessions, startOfWeek, message: "주간 데이터" }, { status: 200 });
     } catch (error) {
         console.error("Error fetching exercise session:", error);
         return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
