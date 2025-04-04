b647d59aa8da9c8f27ee3ad6a346b33f10dcaec3
KIMLENDING <gotslaakaak@gmail.com>
2025-04-04 18:08:15 +0900

    메타데이터 업데이트 및 Open Graph 설정 추가, 불필요한 SVG 파일 삭제, 새로운 아이콘 파일 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,8 +21,29 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "헬스 helper",
-  description: "헬스 도우미 앱",
+  title: "H-Helper | 당신만의 운동 도우미",
+  description: "H-Helper와 함께 건강한 라이프스타일을 만들어보세요.",
+  openGraph: {
+    title: "H-Helper | 당신만의 운동 도우미",
+    description: "H-Helper와 함께 건강한 라이프스타일을 만들어보세요.",
+    url: "https://health-helper-app.vercel.app/",
+    siteName: "H-Helper",
+    images: [
+      {
+        url: "/opengraph-image", // 동적으로 생성된 이미지
+        width: 1200,
+        height: 630,
+        alt: "H-Helper 메인 페이지",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "H-Helper | 당신만의 운동 도우미",
+    description: "개인 맞춤형 운동 프로그램을 경험하세요.",
+    images: ["/opengraph-image"], // 트위터에서도 동일한 동적 이미지 사용
+  },
 };
 
 export default async function RootLayout({
@@ -36,6 +57,7 @@ export default async function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/dumbbell.svg" type="image/svg+xml" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
