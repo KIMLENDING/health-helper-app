8f3e3a3d3def82f7469c4564a95dee80fd8e1495
KIMLENDING <gotslaakaak@gmail.com>
2025-04-29 23:05:59 +0900

    UI 개선: 운동 계획 페이지에서 불필요한 데이터 패칭 및 상태 관리 제거

--- app/dashboard/exercisePlans/page.tsx
+++ app/dashboard/exercisePlans/page.tsx
@@ -1,30 +1,10 @@
 import ShowExercisePlan from '@/components/UserCpmponents/showExercisePlan'
-import { fetchWithCookie } from '@/utils/fetchUrl';
-import getQueryClient from '@/utils/getQueryClient';
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
-import { cookies } from 'next/headers';
-
-
-
-const fetchData = async () => {
-    const cookieHeader = await cookies();
-    const cookieName =
-        process.env.NODE_ENV === "production"
-            ? "__Secure-next-auth.session-token"
-            : "next-auth.session-token";
-    const cookie = cookieHeader.get(cookieName);
-    return await fetchWithCookie(`${process.env.NEXTAUTH_URL}/api/user/exercisePlan`, cookieName, cookie?.value)
-};
 
 const page = async () => {
-    const queryClient = getQueryClient();
-    await queryClient.prefetchQuery({ queryKey: ["exercisePlans"], queryFn: () => fetchData() });
 
     return (
         <div className="flex flex-1 flex-col gap-4 p-4">
-            <HydrationBoundary state={dehydrate(queryClient)}>
-                <ShowExercisePlan />
-            </HydrationBoundary>
+            <ShowExercisePlan />
         </div>
     )
 }
