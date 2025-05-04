8806849f0f034bb8e59cd7ab045b672c96b952d3
KIMLENDING <gotslaakaak@gmail.com>
2025-05-04 18:39:29 +0900

    LeftSideBar 컴포넌트: 코드 정리 및 가독성 향상

--- components/LayoutCompents/LeftSideBar.tsx
+++ components/LayoutCompents/LeftSideBar.tsx
@@ -5,6 +5,7 @@ import { SidebarLeft } from "@/components/UserCpmponents/DynamicComponents"
 // 동적 import로 SidebarLeft 컴포넌트를 불러오기위한 코드
 const LeftSideBar = () => {
     const { openMobile, open } = useSidebar();
+
     return (
         <div>
             {openMobile || open && <SidebarLeft />}
